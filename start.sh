219657fda6ed4fd490d6df28a6aa62bd4f93824a
Cherry00075 <117108582+Cherry00075@users.noreply.github.com>
2022-12-04 19:05:24 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,3 +7,5 @@ kubectl apply -f mongo-deployment-service.yaml
 kubectl apply -f mongo-configmap.yaml
 
 kubectl apply -f mongo-express-deployment-service.yaml
+
+kubectl apply -f dashboard-ingress.yaml
