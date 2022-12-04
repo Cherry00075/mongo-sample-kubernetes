c61b0730aebed189fc3c38cdd1e286b2fbc7c8a0
Cherry00075 <117108582+Cherry00075@users.noreply.github.com>
2022-12-04 18:26:17 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+kubectl apply -f mongodb-secret.yaml
+
+kubectl apply -f mongo-deployment-service.yaml
+
+kubectl apply -f mongo-configmap.yaml
+
+kubectl apply -f mongo-express-deployment-service.yaml
